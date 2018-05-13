70cd0255f4efdf39301cc3eba14ea5ca0ea3d75c
Jakob Schaffarczyk <jaschaffarczyk@gmx.de>
2018-05-13 12:22:24 +0200

    update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,7 +3,12 @@
 [ `whoami` = root ] || { sudo "$0" "$@"; exit $?; }
 DIR="/opt/ctan"
 mkdir $DIR
-cp ctan.sh installed.list new.txt packages.list texmf.txt $DIR
+cp ctan.sh $DIR
 mkdir $DIR/tmp
 chown -R $USER:$USER $DIR
 chmod +x $DIR/ctan.sh
+touch $DIR/installed.list
+touch $DIR/packages.list
+touch $DIR/texmf.txt
+touch $DIR/new.txt
+echo "True" > $DIR/new.txt
